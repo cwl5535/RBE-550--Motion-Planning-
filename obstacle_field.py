018a7188b02542593cbd8a12ae3e3ec616456422
Colton Layhue <layhue497@gmail.com>
2023-01-20 14:22:29 -0500

    add obstacle_field script

---
+++ obstacle_field.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+tetrominoes = [np.array([[1],[1], [1], [1]]), 
+               np.array([[1,1],[0,1],[0,1]]), 
+               np.array([[1,0],[1,1],[0,1]]),
+               np.array([[0,1],[1,1],[0,1]])]
+
+print(tetrominoes[0])
+def add_tetromino(): 
+    pass
