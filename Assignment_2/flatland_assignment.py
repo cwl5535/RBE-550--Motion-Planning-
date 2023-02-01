908f4af3b452c7452462b41b7a8f7a8a5bd88764
Colton Layhue <layhue497@gmail.com>
2023-01-31 23:56:15 -0500

    setting up initial environment

--- Assignment_2/flatland_assignment.py
+++ Assignment_2/flatland_assignment.py
@@ -1,18 +1,16 @@
 from obstacle_field import create_obstacle_field
 import numpy as np
 from matplotlib import pyplot as plt
+import sys
 
+np.set_printoptions(threshold=sys.maxsize)
 
-environment = np.array(
-                [[np.ones((1,128))],
-                [np.ones((126,1)), np.zeros((126,126)), np.ones((126,1))],
-                [np.ones((1,128))]
-                ])
+# Initial Environment with Closed Border
+init_env = np.zeros((128,128))
+init_env[0, 0:128], init_env[1:127, 0], init_env[1:127, 127], init_env[127, 0:128] = 1,1,1,1
 
-print(environment.size, environment.shape)
+obstacle_field, coverage = create_obstacle_field(init_env, 0.05)
 
-# obstacle_field, coverage = create_obstacle_field(environment, 0.7)
+plt.imshow(obstacle_field, cmap= "binary")
 
-# plt.imshow(obstacle_field, cmap= "binary")
-
-# plt.show()
+plt.show()
