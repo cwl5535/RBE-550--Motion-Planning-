3ce471f8dfcb4e91c1edd49bdd82a0f271d9cc3d
Colton Layhue <layhue497@gmail.com>
2023-02-01 23:27:18 -0500

    soft coded the script for grid size

--- Assignment_2/flatland_assignment.py
+++ Assignment_2/flatland_assignment.py
@@ -5,17 +5,23 @@ import sys
 
 np.set_printoptions(threshold=sys.maxsize)
 
+
 # Initial Environment with Closed Border
-init_env = np.zeros((128,128))
+grid_size = 128
+init_env = np.zeros((grid_size, grid_size))
 
 # Adding the borders
-init_env[0, 0:128], init_env[1:127, 0], init_env[1:127, 127], init_env[127, 0:128] = 1,1,1,1
+init_env[0, 0:grid_size], init_env[1:grid_size-1, 0], init_env[1:(grid_size-1), (grid_size-1)], init_env[(grid_size-1), 0:grid_size] = 1,1,1,1
 
-# Generating the obstacles
-obstacle_field, coverage = create_obstacle_field(np.zeros((126,126)), 0.05)  # NOTE that size of environment given to create_obstacle_field is only the area that doesn't include the border from the init_env
+# Generating the obstacles within the area NOT including the border
+obstacle_field, coverage = create_obstacle_field(np.zeros(((grid_size-2),(grid_size-2))), 0.5)  # NOTE that size of environment given to create_obstacle_field is only the area that doesn't include the border from the init_env
+
 # Adding the obstacles to the initial environment
-init_env[1:127, 1:127] = obstacle_field
+init_env[1:(grid_size-1), 1:(grid_size-1)] = obstacle_field
 
+# Displaying the environment
+plt.figure("Assignment 2: Flatland Assignment")
 plt.imshow(init_env, cmap= "binary")
+
 
 plt.show()
