3eb465e68f059a643148ee8d6c5f05a167f56109
Colton Layhue <layhue497@gmail.com>
2023-02-05 16:20:13 -0500

    added statement to end if there are no nodes to explore; added 4 way search instead of 8

--- Assignment_2/flatland_assignment.py
+++ Assignment_2/flatland_assignment.py
@@ -52,16 +52,23 @@ def check_surroundings(environment, current_locations: list) -> list:
         row = location[0]
         col = location[1] 
 
-        surroundings = {(row-1, col-1): environment[row-1, col-1],    (row-1, col): environment[row-1, col],     (row-1, col+1): environment[row-1, col+1],
+        # surroundings = {(row-1, col-1): environment[row-1, col-1],    (row-1, col): environment[row-1, col],     (row-1, col+1): environment[row-1, col+1],
+        #                 (row,   col-1): environment[row,   col-1],                                               (row,   col+1): environment[row,   col+1], 
+        #                 (row+1, col-1): environment[row+1, col-1],    (row+1, col): environment[row+1, col],     (row+1, col+1): environment[row+1, col+1] }
+        
+        surroundings = {                                          (row-1, col): environment[row-1, col],
                         (row,   col-1): environment[row,   col-1],                                               (row,   col+1): environment[row,   col+1], 
-                        (row+1, col-1): environment[row+1, col-1],    (row+1, col): environment[row+1, col],     (row+1, col+1): environment[row+1, col+1] }
+                                                                  (row+1, col+1): environment[row+1, col+1] }
+        
 
+
+        if (all(x!=0 for x in surroundings.values())):
+            # print("No Possible nodes to explore. I am stuck :(")
+            continue
         for key, value in surroundings.items():
             if value == 0:
-                print("Valid Node found!")
+                # print("Valid Node found!")
                 nodes_to_explore.append(key)
-            # else: 
-            #     print("No valid nodes found! Can't walk through walls!")
     
     return nodes_to_explore
     
@@ -75,17 +82,23 @@ def explore_node(location: tuple, environment):
 
 def breadth_first(starting_location: tuple, goal_location: tuple, environment): 
     print("Beginning Breadth First Search!")
+    print(f"Goal Location: {goal_location}")
     current_locations = [starting_location]
     i = 0
     while True: 
         i += 1
+        achieved = False
         # check our surroundings, get a list of eligible locations, change them all to yellow, 
         eligible_nodes = check_surroundings(environment = environment, current_locations= current_locations)
+        if len(eligible_nodes) == 0:
+            print("No Possible nodes to explore. I am stuck :(")
+            break
         for node in eligible_nodes:
             new_env = explore_node(node, environment)
             environment = new_env
             current_location = node
             if current_location == goal_location:
+                achieved = True
                 break
         else:
             current_locations = eligible_nodes
@@ -93,10 +106,27 @@ def breadth_first(starting_location: tuple, goal_location: tuple, environment):
             continue
         break
     
-    print("Goal location has been achieved!")
+    if achieved:
+        print("Goal location has been achieved!")
     environment[goal_location[0], goal_location[1]] = np.nan
     environment[starting_location[0], starting_location[1]] = color_value_for_path
     return new_env, i
+
+
+def create_bordered_env(coverage, grid_size):
+
+    init_env = np.zeros((grid_size, grid_size))
+
+    # Adding the borders
+    init_env[0, 0:grid_size], init_env[1:grid_size-1, 0], init_env[1:(grid_size-1), (grid_size-1)], init_env[(grid_size-1), 0:grid_size] = 1,1,1,1
+
+    # Generating the obstacles within the area NOT including the border
+    obstacle_field, coverage = create_obstacle_field(np.zeros(((grid_size-2),(grid_size-2))), goal_coverage= coverage)  # NOTE that size of environment given to create_obstacle_field is only the area that doesn't include the border from the init_env
+
+    # Adding the obstacles to the initial environment
+    init_env[1:(grid_size-1), 1:(grid_size-1)] = obstacle_field
+
+    return init_env
 
 if __name__ == "__main__":
 
@@ -106,45 +136,70 @@ if __name__ == "__main__":
     # Initial Environment with Closed Border
     grid_size = 128
     color_value_for_path = 0.5
-    init_env = np.zeros((grid_size, grid_size))
-    obstacle_coverage = 0.50
-
-    # Adding the borders
-    init_env[0, 0:grid_size], init_env[1:grid_size-1, 0], init_env[1:(grid_size-1), (grid_size-1)], init_env[(grid_size-1), 0:grid_size] = 1,1,1,1
-
-    # Generating the obstacles within the area NOT including the border
-    obstacle_field, coverage = create_obstacle_field(np.zeros(((grid_size-2),(grid_size-2))), goal_coverage= obstacle_coverage)  # NOTE that size of environment given to create_obstacle_field is only the area that doesn't include the border from the init_env
-
-    # Adding the obstacles to the initial environment
-    init_env[1:(grid_size-1), 1:(grid_size-1)] = obstacle_field
 
 
-    # Placing the Robot
-    starting_row, starting_col, init_env = place_robot("NW", init_env)
 
     # Performing Breadth First Search
+
+    goal_location = (100,100)
+
+    """
+    0% Coverage Calculations
+    """
+    # start_env = create_bordered_env(coverage= 0, grid_size=grid_size)
+    # starting_row, starting_col, init_env = place_robot("NW", start_env)
+    # starting_location = (starting_row, starting_col)
+    # init_env[goal_location[0]-1: goal_location[0]+1, goal_location[1]-1: goal_location[1]+1] = 0  # create a goal area
+
+    # breadth_env_0, iterations_breadth_0 = breadth_first(starting_location=starting_location, goal_location=goal_location, environment=init_env)
+    # print(iterations_breadth_0)
+
+    """
+    25% Coverage Calculations
+    """
+    # start_env = create_bordered_env(coverage= 0.25, grid_size=grid_size)
+    # starting_row, starting_col, init_env = place_robot("NW", start_env)
+    # starting_location = (starting_row, starting_col)
+    # init_env[goal_location[0]-1: goal_location[0]+1, goal_location[1]-1: goal_location[1]+1] = 0  # create a goal area
+
+    # breadth_env_25, iterations_breadth_25 = breadth_first(starting_location=starting_location, goal_location=goal_location, environment=init_env)
+    # print(iterations_breadth_25)
+
+    """
+    50% Coverage Calculations
+    """
+    start_env = create_bordered_env(coverage= 0.250, grid_size=grid_size)
+    starting_row, starting_col, init_env = place_robot("NW", start_env)
     starting_location = (starting_row, starting_col)
-
-    goal_location = (int(24/2), int(24/2))
     init_env[goal_location[0]-1: goal_location[0]+1, goal_location[1]-1: goal_location[1]+1] = 0  # create a goal area
-    final_breadth_env, iterations = breadth_first(starting_location=starting_location, goal_location=goal_location, environment=init_env)
 
+    breadth_env_50, iterations_breadth_50 = breadth_first(starting_location=starting_location, goal_location=goal_location, environment=init_env)
+    print(f"Number of Iterations: {iterations_breadth_50}")
+    """
+    75% Coverage Calculations
+    """
 
+    # start_env = create_bordered_env(coverage= 0.70, grid_size=grid_size)
+    # starting_row, starting_col, init_env = place_robot("NW", start_env)
+    # starting_location = (starting_row, starting_col)
+    # init_env[goal_location[0]-1: goal_location[0]+1, goal_location[1]-1: goal_location[1]+1] = 0  # create a goal area
 
+    # breadth_env_75, iterations_breadth_75 = breadth_first(starting_location=starting_location, goal_location=goal_location, environment=init_env)
+    # print(iterations_breadth_75)
 
-
-
-
-
-    # Displaying the environment
+    # Plots
 
     plt.figure("Assignment 2: Flatland Assignment")
-    plt.title("Breadth First Search")
-
-    cmap = ListedColormap(["white", "yellow", "black"]) # sets 0 as white, 1 as black. See https://stackoverflow.com/questions/68390704/assign-specific-colors-to-values-of-an-array-when-plotting-it-using-imshow-witho
+    plt.suptitle("Breadth First Search")
+    cmap = ListedColormap(["white", "blue", "black"]) # sets 0 as white, 1 as black. See https://stackoverflow.com/questions/68390704/assign-specific-colors-to-values-of-an-array-when-plotting-it-using-imshow-witho
     cmap.set_bad("red")   # sets value that's not 0 or 1 to red. In this case it's np.nan. 
 
-    plt.imshow(final_breadth_env, cmap=cmap)
+    # plt.subplot(2, 4, (1,5)), plt.imshow(breadth_env_0, cmap =  cmap), plt.title("0% Coverage")
+    # plt.subplot(2, 4, (2,6)), plt.imshow(breadth_env_25, cmap = cmap), plt.title("25% Coverage")
+    # plt.subplot(2, 4, (3,7)), plt.imshow(breadth_env_50, cmap = cmap), plt.title("50% Coverage")
+    # plt.subplot(2, 4, (4,8)), plt.imshow(breadth_env_75, cmap = cmap), plt.title("75% Coverage")
+
+    plt.imshow(breadth_env_50, cmap=cmap)
 
 
     plt.show()
