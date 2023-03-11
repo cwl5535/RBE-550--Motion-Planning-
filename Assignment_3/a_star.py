9ae1dd2773c59ed424c06a969a038b381540ed98
Colton Layhue <layhue497@gmail.com>
2023-03-11 01:02:11 -0500

    push commits from desktop

--- Assignment_3/a_star.py
+++ Assignment_3/a_star.py
@@ -1,12 +1,18 @@
+import math
+
 class AStar():
     def __init__(self): 
         self.open = []
         self.past = 0
+    
 
 
 class State():
-    def __init__(self, x, y): 
+    def __init__(self, x, y, theta): 
         self.cost_to_go = 0  # going to be calculated with Euclidean formulas 
         self.x = x
         self.y = y
-        
+        self.theta = theta # possible states are 0 to 2pi
+    def distance_to_goal(self, goal)
+        dist = math.sqrt(self.x**2 + self.y**2)
+
