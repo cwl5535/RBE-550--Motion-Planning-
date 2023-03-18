60d614b0875274cd9849d3603bf98f5fcaebac53
Colton Layhue <layhue497@gmail.com>
2023-03-18 17:26:09 -0400

    working for linear moves in x, change in angle causes oscillations

--- Assignment_3/a_star.py
+++ Assignment_3/a_star.py
@@ -26,6 +26,7 @@ class State():
         self.velocity = velocity
 
     def distance_to(self, to) -> float:
+        # Used to calculate the Euclidean distance between one node to another
         if isinstance(to, tuple):
             delta_x = (self.x - to[0])
             delta_y = (self.x - to[1])
@@ -43,7 +44,7 @@ class State():
 
         # Steering Cost
 
-        steering_cost = 2*abs(self.theta - parent.theta)
+        steering_cost = abs(self.theta - parent.theta) % (2*pi)
 
         total_cost = dist_cost + steering_cost
 
@@ -75,43 +76,60 @@ class AStar():
     def add_to_closed(self, state):
         self.closed.append(state)
 
-    def simulate(self, current_state, car, world):
-        car.velocity = current_state.velocity
-        # print(self.car.velocity)
-        car.inputSteering = current_state.theta
-        world.tick()
-        world.render()
-        time.sleep(0.1/4)
+    def simulate(self, last_state, car, world):
+        # vels = []
+        # steering = []
+
+        state_to_simulate = last_state
+        while state_to_simulate != self.start: 
+            car.velocity = last_state.velocity
+            car.inputSteering = last_state.theta
+            world.tick()
+            world.render()
+            time.sleep(0.1/4)
+            print(f"Velocity: {car.velocity}, Steering Angle: {car.inputSteering}")
+            #vels.append(car.velocity)
+            # steering.append(car.inputSteering)
+        world.close()
+
+
     
     def show_path(self, last_state):
-        path = np.zeros((120,120))
+        path_x = []
+        path_y = []
         state_to_plot = last_state
         while state_to_plot != self.start:
             x = state_to_plot.x
             y = state_to_plot.y
-            path[x][y] = 1
+            path_x.append(x)
+            path_y.append(y)
             state_to_plot = state_to_plot.parent
-        path[self.start.x][self.start.y] = 1
-            # need to take the current one, make the value at its x,y a one, and then do the same for its parent and their parents
+        path_x.append(self.start.x)
+        path_y.append(self.start.y)
 
-        plt.figure("Assignment 3 (RBE 550): Valet")
-        plt.imshow(path, cmap = "binary")
-
+        plt.plot(path_x, path_y)
+        plt.title("Assignment 3 (RBE 550): Valet"), plt.xlim((0,120)), plt.ylim((0,120))
         plt.show()
+
     def collision_check(self, state) -> bool:
         
         for index in range(len(self.obstacles_x)):
             x_left = self.obstacles_x[index][0]  # indexing a tuple 
-            x_right= self.obstacles_x[index][1]
+            x_right = self.obstacles_x[index][1]
             y_left = self.obstacles_y[index][0]
-            y_right= self.obstacles_y[index][1]
+            y_right = self.obstacles_y[index][1]
 
             if (round(state.x) in range(x_left, x_right+1)) and (round(state.y) in range(y_left, y_right + 1)):
                 self.add_to_closed(state)  # state causes collision and is added to closed as a result
                 print("Collision Occurred")
                 return True
         return False
+    
     def plan(self):
+        print("Beginning A* Planning")
+        print(f"Starting Location: {(self.start.x, self.start.y, self.start.theta)}")
+        print(f"Goal Location: {self.goal}")
+
         self.start.h = self.start.calculate_h(self.goal)
         self.start.f = self.start.calculate_f()
 
@@ -123,42 +141,47 @@ class AStar():
 
 
         q = self.start
+        goal_found = False
+        idx = 0
 
-        while len(self.open) > 0:
+        while len(self.open) > 0 or (not goal_found):
+            idx += 1 
+            if idx == 500: 
+                self.show_path(q)
+                break
             print(f"x: {q.x}, y: {q.y}, theta: {q.theta}")
             # print(f"Open list has {len(self.open)} states")
 
             # current = self.open[0]
-            self.simulate(q, self.car, self.world)
+            # self.simulate(q, self.car, self.world)
             self.open.pop(self.open.index(q))
             self.add_to_closed(q)
             # search for the surrounding neighbors
  
-            neighbors = self.get_neighbors(q, timestep=0.5)
+            neighbors = self.get_neighbors(q, timestep = 1)
 
             previous_f = neighbors[0].f
 
             for neighbor in neighbors:
-                if neighbor not in self.closed:
-                        
-                    if self.goal_check(neighbor):
-                        print("Goal found")
-                        self.show_path(q)
-                        break
-                    
-                    else:
-                        neighbor.g = neighbor.calculate_g(neighbor.parent)
-                        neighbor.h = neighbor.calculate_h(self.goal)
-                        neighbor.f = neighbor.calculate_f()
+                if self.goal_check(neighbor):
+                    print("Goal found!")
+                    self.show_path(q)
+                    self.simulate(q, self.car, self.world)
+                    goal_found = True
+                    break
+                elif neighbor not in self.closed:
+                    neighbor.g = neighbor.calculate_g(neighbor.parent)
+                    neighbor.h = neighbor.calculate_h(self.goal)
+                    neighbor.f = neighbor.calculate_f()
                     
                     if neighbor.f < previous_f:
                         q = neighbor
                         previous_f = neighbor.f
-                
+
             self.add_to_open(q)
 
 
-
+        print("Goal has been found, timeout has been reached, or open list is empty")
             # for neighbor in neighbors: 
             #     if neighbor not in self.closed:
                     
@@ -188,6 +211,16 @@ class AStar():
         
         return False
 
+    def exceeds_world_limits(self, x, y) -> bool: 
+        if (x < 0) or (x > self.world.size[0]): 
+            # print("Exceeds limits")
+            return True
+        elif (y < 0) or (y > self.world.size[1]):
+            # print("Exceeds limits")
+            return True
+        else: 
+            return False
+        
     def get_neighbors(self, state, timestep = 0.1) -> List[State]:
         if isinstance(state, State):
             neighbors = []
@@ -195,13 +228,14 @@ class AStar():
             # what are all possible options to go to? I need to vary my position (x,y, theta) with the velocity equations
             # my inputs are for lecture 10, slide 26 is vl and vr. Combinations of each wheel's velocities
             
-            R = 0.1
-            L = 0.4
-            max_speed = 20
+            R = 1
+            L = 4
+
+            max_speed = 5
 
             speeds = []
 
-            for speed in range(1, max_speed+1): # looping to get all possible speed combos for the two wheels
+            for speed in range(-max_speed, max_speed+1): # looping to get all possible speed combos for the two wheels
                 speeds.append((speed, max_speed))
                 speeds.append((max_speed, speed))
             
@@ -210,9 +244,9 @@ class AStar():
             for speed in speeds: 
                 # calculating velocity and position with each speed combo
                 vr, vl = speed[0], speed[1]
-                            
+                        
                 theta_dot = (R/L)*(vr - vl)
-                theta =  state.theta + (theta_dot*timestep) 
+                theta =  radians(state.theta + (theta_dot*timestep))
 
                 x_dot = (R/2)*(vr + vl)*cos(theta)
                 x = state.x + (x_dot * timestep)
@@ -221,10 +255,16 @@ class AStar():
                 y_dot = (R/2)*(vr + vl)*sin(theta)
                 y = state.y + (y_dot * timestep)
 
-                print(x_dot, y_dot, theta_dot)
-                neighbor_state = State(x, y, theta, velocity= Point(x_dot, y_dot))
-                neighbor_state.parent = state
-                neighbors.append(neighbor_state)
+                # print(x_dot, y_dot, theta_dot)
+
+                if self.exceeds_world_limits(x, y): 
+                    continue
+                else:
+                    neighbor_state = State(x, y, theta, velocity= Point(x_dot, y_dot))
+                    neighbor_state.vr = vr
+                    neighbor_state.vl = vl
+                    neighbor_state.parent = state
+                    neighbors.append(neighbor_state)
             
             return neighbors
         else:
