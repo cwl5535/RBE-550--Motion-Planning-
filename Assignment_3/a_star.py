e68fd1c74428be86158dba7a152514b9bc40b9c7
Colton Layhue <layhue497@gmail.com>
2023-03-19 22:36:43 -0400

    updated plots to include obstacles

--- Assignment_3/a_star.py
+++ Assignment_3/a_star.py
@@ -1,12 +1,10 @@
 from math import sqrt, pi, inf, radians, cos, sin
 from typing import List, Tuple
 from matplotlib import pyplot as plt
+from matplotlib.patches import Rectangle
 import numpy as np
 import time
 from geometry import Point
-from world import World
-from agents import Car, RectangleBuilding
-from create_world import create_world
 
 # create world with obstacles >> have a way to access the x and y of obstacles
 
@@ -80,7 +78,7 @@ class AStar():
     def simulate(self, path_x, path_y, path_theta):
 
         print("Beginning Simulation...")
-        time.sleep(5.)
+        time.sleep(2.)
         
         for i in range(len(path_x)): 
             self.car.center = Point(path_x[i], path_y[i])
@@ -110,12 +108,42 @@ class AStar():
         path_y.insert(0,self.start.y)
         path_theta.insert(0, self.start.theta)
 
-        plt.plot(path_x, path_y, linewidth = 4)
-        plt.plot(self.start.x, self.start.y, color = "red", marker = "o", markersize = 8)
-        plt.plot(self.goal[0], self.goal[1], color = "green", marker = "o", markersize = 8)
-        plt.legend(["Path", "Start", "Goal"])
+        fig, ax = plt.subplots()
+        # plotting the path
+        ax.plot(path_x, path_y, linewidth = 4)
 
-        plt.title("Assignment 3 (RBE 550): Valet"), plt.xlim((0,self.world.size[0])), plt.ylim((0,self.world.size[1]))
+        # adding start and goal points to plot
+        ax.plot(self.start.x, self.start.y, color = "red", marker = "o", markersize = 8)
+        ax.plot(self.goal[0], self.goal[1], color = "green", marker = "o", markersize = 8)
+
+        # adding the obstacles to the plot 
+        for obstacle in range(len(self.obstacles_x)):
+            plot_x_left = self.obstacles_x[obstacle][0]  # indexing a tuple 
+            plot_x_right = self.obstacles_x[obstacle][1]
+            plot_y_left = self.obstacles_y[obstacle][0]
+            plot_y_right = self.obstacles_y[obstacle][1]
+
+            width = plot_x_right - plot_x_left
+            height = plot_y_right - plot_y_left
+
+
+            ax.add_patch(Rectangle((plot_x_left,plot_y_left), width, height, color = "black"))
+
+        ax.legend(["Path", "Start", "Goal", "Obstacles"])
+
+
+
+
+        if self.car.vehicle_type == "skid":
+            ax.set_title("Valet: Delivery Robot")  
+        if self.car.vehicle_type == "car":
+            ax.set_title("Valet: Police Car")  
+        if self.car.vehicle_type == "truck":
+            ax.set_title("Valet: Truck with Trailer ")   
+
+        plt.xlim((0,self.world.size[0]))
+        plt.ylim((0,self.world.size[1]))
+        fig.canvas.manager.set_window_title("Assignment 3 (RBE 550): Valet")
         plt.show()
         return path_x, path_y, path_theta
 
@@ -129,12 +157,19 @@ class AStar():
             y_right = self.obstacles_y[index][1]
 
             if (round(x) in range(x_left-buffer, x_right + 1 + buffer)) and (round(y) in range(y_left - buffer, y_right + 1 + buffer)):
-                print("Collision Occurred")
+                # print("Collision Occurred")
                 return True
         return False
     
     def plan(self):
-        print("Beginning A* Planning")
+        if self.car.vehicle_type == "skid":
+            print("Beginning A* Planning with the Delivery Robot!")
+        if self.car.vehicle_type == "car":
+            print("Beginning A* Planning with the Police Car!")
+        if self.car.vehicle_type == "truck":
+            print("Beginning A* Planning with the Truck and Trailer!")
+    
+
         print(f"Starting Location: {(self.start.x, self.start.y, self.start.theta)}")
         print(f"Goal Location: {self.goal}")
 
@@ -160,8 +195,8 @@ class AStar():
                 self.simulate(path_x, path_y, path_theta)
                 print("Loop limit has been achieved!")
                 break
-            if idx % 50 == 0: 
-                print(f"x: {q.x}, y: {q.y}, theta: {q.theta}")
+            # if idx % 1000 == 0: 
+            #     print(f"x: {q.x}, y: {q.y}, theta: {q.theta}")
 
             # Main Algorithm
 
@@ -236,7 +271,7 @@ class AStar():
             return False
         
     def get_neighbors(self, state, timestep = 0.1) -> List[State]:
-        if isinstance(state, State):
+        if self.car.vehicle_type == "skid":
             neighbors = []
 
             # what are all possible options to go to? I need to vary my position (x,y, theta) with the velocity equations
@@ -283,10 +318,107 @@ class AStar():
                     neighbor_state.vl = vl
                     neighbor_state.parent = state
                     neighbors.append(neighbor_state)
+        elif self.car.vehicle_type == "car":
+            neighbors = []
+
+            # what are all possible options to go to? I need to vary my position (x,y, theta) with the velocity equations
+            # my inputs are for lecture 10, slide 26 is vl and vr. Combinations of each wheel's velocities
             
-            return neighbors
+            R = 1 # wheel radius
+            L = 4  # distance between wheels
+
+            max_speed = 5
+
+            speeds = []
+
+            for speed in range(-max_speed, max_speed+1): # looping to get all possible speed combos for the two wheels
+                speeds.append((speed, max_speed))
+                speeds.append((max_speed, speed))
+            
+            speeds.sort()
+            speeds.pop()
+            
+            for speed in speeds: 
+                # calculating velocity and position with each speed combo
+                vr, vl = speed[0], speed[1]
+                        
+                theta_dot = (R/L)*(vr - vl)
+                theta =  state.theta + (theta_dot*timestep)  # should already be in radians
+
+                theta = round(theta % (2*pi),2)
+
+                x_dot = (R/2)*(vr + vl)*cos(theta)
+                x = round(state.x + (x_dot * timestep),2)
+                
+
+                y_dot = (R/2)*(vr + vl)*sin(theta)
+                y = round(state.y + (y_dot * timestep),2)
+
+                # print(f" (x_dot, y_dot, theta_dot): {x_dot, y_dot, theta_dot}")
+
+                if self.exceeds_world_limits(x, y) or self.collision_check(x, y): 
+                    # if either condition happens, we avoid even creating a State for invalid coordinates
+                    continue
+                else:
+                    neighbor_state = State(x, y, theta, velocity= Point(x_dot, y_dot))
+                    neighbor_state.vr = vr
+                    neighbor_state.vl = vl
+                    neighbor_state.parent = state
+                    neighbors.append(neighbor_state)
+        elif self.car.vehicle_type == "truck":
+            neighbors = []
+
+            # what are all possible options to go to? I need to vary my position (x,y, theta) with the velocity equations
+            # my inputs are for lecture 10, slide 26 is vl and vr. Combinations of each wheel's velocities
+            
+            R = 1 # wheel radius
+            L = 4  # distance between wheels
+
+            max_speed = 5
+
+            speeds = []
+
+            for speed in range(-max_speed, max_speed+1): # looping to get all possible speed combos for the two wheels
+                speeds.append((speed, max_speed))
+                speeds.append((max_speed, speed))
+            
+            speeds.sort()
+            speeds.pop()
+            
+            for speed in speeds: 
+                # calculating velocity and position with each speed combo
+                vr, vl = speed[0], speed[1]
+                        
+                theta_dot = (R/L)*(vr - vl)
+                theta =  state.theta + (theta_dot*timestep)  # should already be in radians
+
+                theta = round(theta % (2*pi),2)
+
+                x_dot = (R/2)*(vr + vl)*cos(theta)
+                x = round(state.x + (x_dot * timestep),2)
+                
+
+                y_dot = (R/2)*(vr + vl)*sin(theta)
+                y = round(state.y + (y_dot * timestep),2)
+
+                # print(f" (x_dot, y_dot, theta_dot): {x_dot, y_dot, theta_dot}")
+
+                if self.exceeds_world_limits(x, y) or self.collision_check(x, y): 
+                    # if either condition happens, we avoid even creating a State for invalid coordinates
+                    continue
+                else:
+                    neighbor_state = State(x, y, theta, velocity= Point(x_dot, y_dot))
+                    neighbor_state.vr = vr
+                    neighbor_state.vl = vl
+                    neighbor_state.parent = state
+                    neighbors.append(neighbor_state)
         else:
             return 0
+        return neighbors
+
+
+
+
 
 
 
