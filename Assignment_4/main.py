0f5bd59cdf0a231a7e4042af81fdc975e30a7ce0
Colton Layhue <layhue497@gmail.com>
2023-03-26 21:20:58 -0400

    push to PC

--- Assignment_4/main.py
+++ Assignment_4/main.py
@@ -2,9 +2,12 @@ from obstacle_field import create_obstacle_field
 import numpy as np
 from matplotlib import pyplot as plt
 
-field_size = 50
+field_size = 250
 
-obstacle_field, coverage = create_obstacle_field(np.zeros((field_size, field_size)), goal_coverage = 0.1)
+obstacle_field, coverage = create_obstacle_field(
+    environment = np.zeros((field_size, field_size)), 
+    goal_coverage = 0.1, 
+    obstacle_square_unit= 5)
 
 plt.figure("Assignment 4 (RBE 550): Firetruck vs. Wumpus")
 # plt.subplot(2, 3, (1,4)), 
