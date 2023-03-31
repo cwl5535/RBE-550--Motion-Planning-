78deb84773a119196345f72c8bb42a118c00aaf2
Colton Layhue <layhue497@gmail.com>
2023-03-31 19:07:32 -0400

    push prm to desktop

--- Assignment_4/main.py
+++ Assignment_4/main.py
@@ -3,7 +3,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os
 import sys
-from PythonRobotics.PathPlanning.ProbabilisticRoadMap import probabilistic_road_map
+from PythonRobotics.PathPlanning.ProbabilisticRoadMap import probabilistic_road_map as prm
+
+
+# Creating an Obstacle Field 
+#############################################
 
 # field_size = 250
 
@@ -14,6 +18,8 @@ from PythonRobotics.PathPlanning.ProbabilisticRoadMap import probabilistic_road_
 
 # os.chdir(r"C:\Users\layhu\Desktop\RBE-550--Motion-Planning-\Assignment_4")
 # np.save("10_coverage3.npy", obstacle_field)
+
+################################################################
 
 
 # plt.figure("Assignment 4 (RBE 550): Firetruck vs. Wumpus")
@@ -31,13 +37,8 @@ if os.getcwd() is not path:
 
 
 np.set_printoptions(threshold=sys.maxsize)
-# print(start_env)
-
-# plt.imshow(start_env, cmap = "binary"), plt.title(f"{10} % Coverage")
-plt.show()
 
 iterations = np.nditer(start_env, flags=["multi_index"])
-
 
 obstacle_x = []
 obstacle_y = []
@@ -55,10 +56,39 @@ for i in iterations:
         open_y.append(iterations.multi_index[1])
 
 
-probabilistic_road_map.prm_planning()
-# print(f"x locations: {obstacle_x}, y locations: {obstacle_y}")
+
+def main(rng=None):
+
+    print(__file__ + " start!!")
+    show_animation = True
+    # start and goal position
+    sx = 10.0  # [m]
+    sy = 10.0  # [m]
+    gx = 50.0  # [m]
+    gy = 50.0  # [m]
+    robot_size = 1.0  # [m]
+
+    ox = obstacle_x
+    oy = obstacle_y
+
+    if show_animation:
+        plt.plot(ox, oy, ".k")
+        plt.plot(sx, sy, "^r")
+        plt.plot(gx, gy, "^c")
+        plt.grid(True)
+        plt.axis("equal")
+
+    rx, ry = prm.prm_planning(sx, sy, gx, gy, ox, oy, robot_size)
+
+    assert rx, 'Cannot found path'
+
+    if True:
+        plt.plot(rx, ry, "-r")
+        plt.pause(0.001)
+        plt.show()
 
 
+if __name__ == '__main__':
+    main()
 
 
-# plt.show()
